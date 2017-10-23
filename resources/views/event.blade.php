8583d4ca648b8c8a6d109d3b78478f97588320b0
Jonas DF <defrere.jonas@gmail.com>
2017-10-23 23:07:33 +0200

    Added unversioned views and HomeContoller

---
+++ resources/views/event.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Event X</div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        Nothing to see here
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        Nothing to see here
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        Nothing to see here
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
