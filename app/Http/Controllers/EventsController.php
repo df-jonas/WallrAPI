3b267c2012901d21587790470f23b9dee490c199
Jonas DF <defrere.jonas@gmail.com>
2017-10-23 23:04:47 +0200

    Added API functions and auth

---
+++ app/Http/Controllers/EventsController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jonas
+ * Date: 23/10/2017
+ * Time: 22:26
+ */
+
+namespace App\Http\Controllers;
+
+
+class EventsController extends Controller
+{
+    public function GetAll()
+    {
+
+    }
+
+    public function GetOne()
+    {
+
+    }
+
+    public function Create()
+    {
+
+    }
+
+    public function Update()
+    {
+
+    }
+}
