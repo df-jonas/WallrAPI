95619b46b67ae1ca2a1b28566501517cbc77e153
Jonas DF <defrere.jonas@gmail.com>
2017-10-24 19:03:41 +0200

    Implemented controllers for Users and Events

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -9,30 +9,83 @@
 namespace App\Http\Controllers;
 
 
+use App\Event;
+use App\Helpers\SecurityHelper;
+use App\Text;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 class EventsController extends Controller
 {
     public function Index()
     {
-
+        return view('event');
     }
 
     public function GetAll()
     {
-
+        return Auth::user()->events()->get();
     }
 
-    public function GetOne()
+    public function GetOne($id)
     {
-
+        if (isset($id)) {
+            if (Auth::user()->events()->find($id) != null) {
+                return Auth::user()->events()->where('id', $id)->with('texts')->get();
+            }
+            return response()->json(['statuscode' => 401, 'message' => "This event may not exist or is not yours."], 401);
+        }
+        return response()->json(['statuscode' => 400, 'message' => "@param 'id' not present."], 400);
     }
 
-    public function Create()
+    public function Create(Request $request)
     {
+        if (isset($request->name) && !isEmptyOrNullString($request->name)) {
 
+            $event = new Event;
+
+            $event->user_id = Auth::user()->id;
+            $event->name = $request->name;
+            $event->public_event_id = $this->getUniquePublicID();
+
+            $event->save();
+
+            return $event;
+        }
+
+        return response()->json(['statuscode' => 400, 'message' => "Some @params were not present."], 400);
     }
 
-    public function Update()
+    public function Update(Request $request, $id)
     {
+        if (isset($id)) {
+            $event = Auth::user()->events()->find($id);
+            if ($event != null) {
 
+                if (isset($request->name) && !isEmptyOrNullString($request->name)) {
+                    $event->name = $request->name;
+                }
+
+                if (isset($request->renew) && $request->renew == "y") {
+                    $event->public_event_id = $this->getUniquePublicID();
+                }
+
+                $event->save();
+
+                return $event;
+            }
+            return response()->json(['statuscode' => 401, 'message' => "This event may not exist or is not yours."], 401);
+        }
+        return response()->json(['statuscode' => 400, 'message' => "@param 'id' not present."], 400);
+    }
+
+    private function getUniquePublicID()
+    {
+        $public_event_id = SecurityHelper::generateRandom(16);
+        while (Event::query()->where('public_event_id', '=', $public_event_id)->first() != null) {
+            $public_event_id = SecurityHelper::generateRandom(16);
+        }
+        return $public_event_id;
     }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,14 +9,35 @@
 namespace App\Http\Controllers;
 
 
+use App\Helpers\SecurityHelper;
 use App\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
     public function Register()
     {
-        return Auth::user();
+        if (isset($request->device_name)) {
+
+            $user = new User;
+            $user->device_name = $request->device_name;
+            $user->device_uuid = SecurityHelper::generateRandom(60);
+            $user->device_verifytoken = SecurityHelper::generateRandom(60);
+            $user->api_token = SecurityHelper::generateRandom(60);
+
+            $user->save();
+
+            return response()->json(array(
+                "id" => $user->id,
+                "device_name" => $user->device_name,
+                "device_uuid" => $user->device_uuid,
+                "device_verifytoken" => $user->device_verifytoken,
+                "api_token" => $user->api_token,
+            ));
+        }
+
+        return response()->json(['statuscode' => 400, 'message' => "@param 'device_name' not present."], 400);
     }
 
     public function Me()
